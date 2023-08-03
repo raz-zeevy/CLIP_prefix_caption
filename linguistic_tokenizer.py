afe018ba46ebaa88430a7b500768197866231887
Raz Zeevy <raz3zeevy@gmail.com>
2023-08-03 12:42:30 +0300

    small fix

--- linguistic_tokenizer.py
+++ linguistic_tokenizer.py
@@ -18,17 +18,16 @@ TAG_START = '<#'
 TAG_END = '#> '
 
 
-def create_tag(tag):
+def create_tag(tag) -> str:
     return TAG_START + TAGS_SYMBOLS[tag] + TAG_END
 
 
 def create_tokenizer(gpt2_type: str):
     from transformers import GPT2Tokenizer
     tokenizer = GPT2Tokenizer.from_pretrained(gpt2_type)
-    for tag in TAGS:
-        tokenizer.add_special_tokens(
-            {'additional_special_tokens' : create_tag(tag)}
-        )
+    tokenizer.add_special_tokens(
+        {'additional_special_tokens' : [create_tag(tag) for tag in TAGS]}
+    )
     return tokenizer
 
 
